0e43c89cdb91f3fd7432e67afcc1e7597343d199
Gabor Kukucska <gabor@buyahmedia.com>
2024-02-24 23:01:43 +0800

    Update constants.py

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -1,18 +1,10 @@
 import torch
 
 PUBLIC_INITIAL_PEERS = [
-    # IPv4 DNS addresses
-    "/dns/bootstrap1.petals.dev/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
-    "/dns/bootstrap2.petals.dev/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
-    # IPv6 DNS addresses
-    "/dns6/bootstrap1.petals.dev/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
-    "/dns6/bootstrap2.petals.dev/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
-    # Reserved IPs
-    "/ip4/159.89.214.152/tcp/31337/p2p/QmedTaZXmULqwspJXz44SsPZyTNKxhnnFvYRajfH7MGhCY",
-    "/ip4/159.203.156.48/tcp/31338/p2p/QmQGTqmM7NKjV6ggU1ZCap8zWiyKR89RViDXiqehSiCpY5",
+    "/ip4/192.168.1.29/tcp/31337/p2p/QmaRgjKczTzBE3Dup3BQZWGZ2YbzJzsuLykJdp7fBGnJgp",
 ]
 
 # The reachability API is currently used only when connecting to the public swarm
-REACHABILITY_API_URL = "https://health.petals.dev"
+REACHABILITY_API_URL = "http://192.168.1.27:1111"
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
