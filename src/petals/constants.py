2b3adc8a3f746bcad43ac2cbafbe7182dba9d394
Gabor Kukucska <gabor@buyahmedia.com>
2024-05-28 17:30:40 +0800

    Update constants.py
    AWS cpu

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -1,10 +1,10 @@
 import torch
 
 PUBLIC_INITIAL_PEERS = [
-    "/ip4/192.168.0.14/tcp/31337/p2p/QmcTB9tM37HrKWDaCKEDGtbiGYEpN8mqNcBiNnoF929wWM",
+    "/ip4/ai.gettingitalldone.com/tcp/31337/p2p/QmTbqDrBxCioZMYCjTUHu5GLVERw369VkY7fBTMiKFFDXu",
 ]
 
 # The reachability API is currently used only when connecting to the public swarm
-REACHABILITY_API_URL = "http://192.168.0.14:1111"
+REACHABILITY_API_URL = "http://health.gettingitalldone.com"
 
 DTYPE_MAP = dict(bfloat16=torch.bfloat16, float16=torch.float16, float32=torch.float32, auto="auto")
