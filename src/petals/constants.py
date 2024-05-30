8e9249b6886de2274c7cb4421ad268fab2e2bfed
Gabor Kukucska <gabor@buyahmedia.com>
2024-05-30 14:11:32 +0800

    Update constants.py

--- src/petals/constants.py
+++ src/petals/constants.py
@@ -1,7 +1,8 @@
 import torch
 
 PUBLIC_INITIAL_PEERS = [
-    "/dns/ai.gettingitalldone.com/tcp/31337/p2p/QmTbqDrBxCioZMYCjTUHu5GLVERw369VkY7fBTMiKFFDXu",
+#    "/dns/ai.gettingitalldone.com/tcp/31337/p2p/QmTbqDrBxCioZMYCjTUHu5GLVERw369VkY7fBTMiKFFDXu",
+    "/ip4/192.168.0.14/tcp/31337/p2p/QmcTB9tM37HrKWDaCKEDGtbiGYEpN8mqNcBiNnoF929wWM",
 ]
 
 # The reachability API is currently used only when connecting to the public swarm
